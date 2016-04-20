caad13209343d4a9031e0a361cd262c8976f1d82
Rick Henry <rick.henry@codeclan.com>
2016-04-20 17:05:37 +0100

    basic canvas achieved

--- client/source/index.js
+++ client/source/index.js
@@ -5,7 +5,7 @@ var Team = require( './match/team' );
 
 window.onload = function() {
 
-  var player1 = Player( {name:'Rick Henry', passing: 20} ).setPos( 10, 10 )
+  var player1 = Player( {name:'Rick Henry', passing: 20} ).setPos( 10, 10 );
   player1.gainPossession();
   var player2 = Player( {name:'Leo Messi', passing: 16} ).setPos( 50, 50 );
   var player3 = Player( {name:'Jon Henry', passing: 15} ).setPos( 100, 100 );
@@ -15,29 +15,35 @@ window.onload = function() {
   team.addPlayer( player2 );
   team.addPlayer( player3 );
 
-  var i = 0;
+  team.pass();
 
-  while (i < 10) {
-    team.pass();
-    i++
+  var ball = Ball();
+
+  var pitch = document.getElementById( 'pitch' );
+  var ctx = pitch.getContext( '2d' );
+
+  var drawPlayer = function( x, y ) {
+    ctx.beginPath();
+    ctx.arc(x,y,10,0,Math.PI*2,true); // Outer circle
+    ctx.stroke();
   }
 
-  // var ball = Ball();
+  var drawBall = function( x, y ) {
+    ctx.beginPath();
+    ctx.arc( player.posX + 15, player.posY, ball.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
 
-  // var pitch = document.getElementById( 'pitch' );
-  // var ctx = pitch.getContext( '2d' );
+  var drawLine = function() {
+    
+  }
 
-  // for ( player of team.players ) {
-  //   ctx.beginPath();
-  //   ctx.arc( player.posX, player.posY, 10, 0, 2 * Math.PI);
-  //   ctx.stroke();
-
-  //   if (player.possession) {
-  //     ctx.beginPath();
-  //     ctx.arc( player.posX + 15, player.posY, ball.size, 0, 2 * Math.PI);
-  //     ctx.stroke();
-  //   }
-  // }
+  for ( player of team.players ) {
+    drawPlayer( player.posX, player.posY ); 
+    if ( player.possession ) {
+      drawBall( player.posX, player.posY );
+    }
+  }
 
 
 
